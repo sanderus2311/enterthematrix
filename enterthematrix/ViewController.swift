953ff667d00c6c3018f797d20686d8eda44c948e
Base <Base@Bases-MacBook-Pro.local>
2014-11-15 14:42:17 +0100

    Method for parse matrix to array

--- enterthematrix/ViewController.swift
+++ enterthematrix/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var matrixHeaders: [UILabel]!
+    @IBOutlet var firstMatrix: [UITextView]!
+    @IBOutlet var secondMatrix: [UITextView]!
+    
+    @IBOutlet weak var plusBtn: UIButton!
+    @IBOutlet weak var minusBtn: UIButton!
+    @IBOutlet weak var multiplyBtn: UIButton!
 
+    var firstMatrixArray : [Double] = []
+    var secondMatrixArray : [Double] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.plusBtn.addTarget(self, action:"parseMatrix", forControlEvents: UIControlEvents.TouchUpInside)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
-
+    func parseMatrix() {
+        var index  = 0;
+        for textView in self.firstMatrix as [UITextView] {
+            var tempString = (textView.text as NSString).doubleValue
+            self.firstMatrixArray.append(tempString)
+            index += 1
+        }
+        
+        for textView in self.secondMatrix as [UITextView] {
+            var tempString = (textView.text as NSString).doubleValue
+            self.secondMatrixArray.append(tempString)
+            index += 1
+        }
+        
+        println("Our first matrix is: \(self.firstMatrixArray) \nOur second matrix is: \(self.secondMatrixArray)")
+    }
+    
 }
 
---
+++ enterthematrix/thematrix.swift
@@ -0,0 +1,13 @@
+//
+//  thematrix.swift
+//  enterthematrix
+//
+//  Created by Base on 11/15/14.
+//  Copyright (c) 2014 Base. All rights reserved.
+//
+
+import UIKit
+
+class thematrix: NSObject {
+   
+}
